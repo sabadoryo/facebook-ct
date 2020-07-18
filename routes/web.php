2d818aa33a2958ffd42493b92480024db0fe55fe
Yernar Abilbay <eabilbay@gmail.com>
2020-07-18 19:46:27 +0600

    asdasdasdsa

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('{any}', 'AppController@index')
+    ->where('any','.*')
+    ->middleware('auth')
+    ->name('home');
